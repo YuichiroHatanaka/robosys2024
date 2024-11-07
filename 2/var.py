af0b877f2cca9d97d951b047eb1e374dc3b8e5ae
Yuichiro Hatanaka <s19c1092el@s.chibakoudai.jp>
2024-11-07 11:22:20 +0900

    python

--- 2/var.py
+++
@@ -1,5 +0,0 @@
-name = "hata"
-tmp = name
-money = 5       #5という数字にmoneyという名前をつける
-print("{}の所持金: {}円".format(name, money) ) #文字列に対してformatメソッドを呼び出し
-print("{}の所持金: {}円".format(tmp, money) ) #文字列に対してformatメソッドを呼び出し
